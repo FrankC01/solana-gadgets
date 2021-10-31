deb3c9cfb036c4652c8265dd53b02563557b5fab
Frank V. Castellucci <frankiecast@gmail.com>
2021-10-31 17:22:22 -0400

    Some unit testing

---
+++ python/sad/tests/unit/test_datadeser.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+import pytest
+from src.datadeser import Tree, load_deserializer
+import yaml
+from yaml.cyaml import CSafeLoader
+
+
+def test_simple_tree_pass():
+    in_dict = {
+        "foo": [
+            {
+                "initialized": {
+                    "type": "Bool",
+                    "serialized": False
+                }
+            }
+        ]
+    }
+    Tree(in_dict)
+
+
+def test_simple_tree_fail():
+    in_dict = {
+        "foo":
+            {
+                "initialized": {
+                    "type": "Bool",
+                    "serialized": False
+                }
+            }
+    }
+    with pytest.raises(AttributeError):
+        Tree(in_dict)
+
+
+def test_tree_load_pass():
+    valid_path = Path(
+        "descriptors/SampGgdt3wioaoMZhC6LTSbg4pnuvQnSfJpDYeuXQBv.yml")
+    with open(valid_path) as f:
+        Tree(yaml.load(f, Loader=CSafeLoader))
+
+
+def test_load_file_pass():
+    valid_path = Path(
+        "descriptors/SampGgdt3wioaoMZhC6LTSbg4pnuvQnSfJpDYeuXQBv.yml")
+    load_deserializer(valid_path)
+
+
+def test_load_file_fail():
+    invalid_path = Path(
+        "SampGgdt3wioaoMZhC6LTSbg4pnuvQnSfJpDYeuXQBv.yml")
+    with pytest.raises(FileNotFoundError):
+        load_deserializer(invalid_path)
