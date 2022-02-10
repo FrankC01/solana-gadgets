42c94ef2dfb272da22d7537edb5f826149872632
Frank V. Castellucci <frankiecast@gmail.com>
2022-02-10 07:01:31 -0500

    Added solana-test-validaator deactivate feature output option

--- rust/scfsd/src/clparse.rs
+++ rust/scfsd/src/clparse.rs
@@ -17,6 +17,20 @@ pub fn build_command_line_parser() -> App<'static> {
                 .default_value("all")
                 .help("Clusters to analyze"),
         )
+        .arg(
+            Arg::new("keys_only_for_inactive")
+                .long("keys-only-for-inactive")
+                .multiple_occurrences(false)
+                .short('k')
+                .help("Generates list of inactivated feature keys for specific cluster (-c) of devnet, testnet or mainnet"),
+        )
+        .arg(
+            Arg::new("target-test-validator")
+                .long("target-test-validator")
+                .multiple_occurrences(false)
+                .short('t')
+                .help("Generates list of inactivated feature keys for input to solana-test-validator"),
+        )
 }
 
 #[cfg(test)]
@@ -49,5 +63,53 @@ mod tests {
         assert_eq!(matches.occurrences_of("cluster"), 1);
         assert_eq!(matches.occurrences_of("cluster") as usize, inc_set.len());
         assert!(inc_set.contains("devnet"));
+    }
+    #[test]
+    fn test_keys_inactive_pass() {
+        let match_res =
+            build_command_line_parser().try_get_matches_from(["fing", "-k", "-c", "devnet"]);
+        assert!(match_res.is_ok());
+        let matches = match_res.unwrap();
+        let mut inc_set = HashSet::<&str>::new();
+        inc_set.extend(matches.values_of("cluster").unwrap());
+        let safe_inacts_only = if matches.is_present("keys_only_for_inactive") && inc_set.len() == 1
+        {
+            if inc_set.contains("all") || inc_set.contains("local") {
+                false
+            } else {
+                true
+            }
+        } else {
+            false
+        };
+        assert!(safe_inacts_only);
+    }
+    #[test]
+    fn test_keys_no_inactive_pass() {
+        let match_res = build_command_line_parser().try_get_matches_from(["fing"]);
+        assert!(match_res.is_ok());
+        let matches = match_res.unwrap();
+        assert_eq!(matches.is_present("keys_only_for_inactive"), false);
+    }
+
+    #[test]
+    fn test_keys_inactive_fail() {
+        let match_res = build_command_line_parser()
+            .try_get_matches_from(["fing", "-k", "-c", "devnet", "-c", "testnet"]);
+        assert!(match_res.is_ok());
+        let matches = match_res.unwrap();
+        let mut inc_set = HashSet::<&str>::new();
+        inc_set.extend(matches.values_of("cluster").unwrap());
+        let safe_inacts_only = if matches.is_present("keys_only_for_inactive") && inc_set.len() == 1
+        {
+            if inc_set.contains("all") || inc_set.contains("local") {
+                false
+            } else {
+                true
+            }
+        } else {
+            false
+        };
+        assert!(!safe_inacts_only);
     }
 }
--- rust/scfsd/src/main.rs
+++ rust/scfsd/src/main.rs
@@ -17,21 +17,36 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = build_command_line_parser().get_matches();
     let mut inc_set = HashSet::<&str>::new();
     inc_set.extend(matches.values_of("cluster").unwrap());
-    let matrix_result = if inc_set.contains("all") {
+    let mut matrix_result = if inc_set.contains("all") {
         ScfsMatrix::new(None)
     } else {
         ScfsMatrix::new(Some(ScfsCriteria {
             clusters: Some(inc_set.iter().map(|cluster| cluster.to_string()).collect()),
             ..Default::default()
         }))
-    };
+    }?;
 
-    if matrix_result.is_ok() {
-        let mut matrix = matrix_result.unwrap();
-        let run_result = matrix.run();
-        if run_result.is_ok() {
-            write_matrix_stdio(&matrix)
+    matrix_result.run()?;
+    if matches.is_present("keys_only_for_inactive") && inc_set.len() == 1 {
+        let test_for: HashSet<_> = ["all", "local"].iter().cloned().collect();
+        let intersection: HashSet<_> = test_for.intersection(&inc_set).collect();
+        if intersection.len() == 0 {
+            // Fetch the keys only
+            let inactives = matrix_result.get_features(Some(&ScfsMatrix::all_inactive))?;
+            let istr: Vec<String> = inactives.iter().map(|f| f.to_string()).collect();
+            // If target is input to solana-test-validator
+            if matches.is_present("target-test-validator") {
+                let mut rvec = vec!["--deactivate-feature".to_string()];
+                rvec.push(istr.join(" --deactivate-feature "));
+                println!("{}", rvec.join(" "));
+            } else {
+                println!("{}", istr.join(" "));
+            }
+        } else {
+            println!("Error: -k can only be used with -c devnet OR -c testnet OR -c mainnet. Found -c {:?}", intersection);
         }
+    } else {
+        write_matrix_stdio(&matrix_result);
     }
 
     Ok(())
